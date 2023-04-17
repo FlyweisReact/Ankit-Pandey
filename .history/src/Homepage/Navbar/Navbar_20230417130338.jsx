ab9a167db0ac3c5d240f092a1abba64aa1310cdb
React1 <React1@flyweis.technology>
2023-04-17 16:56:36 +0530

    Ankit Pandey

---
+++ .history/src/Homepage/Navbar/Navbar_20230417130338.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import "./Navbar.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import Dropdown from "react-bootstrap/Dropdown";
+import { IoMdArrowDropdown } from "react-icons/io";
+import {}
+
+const Navbar = ({ width, setWidth }) => {
+  const Live = () => {
+    let defaultAccount = document.getElementById("defaultAccount");
+    defaultAccount.style.display = "none";
+
+    let Demo = document.getElementById("DemoAcccount");
+    Demo.style.display = "none";
+
+    let target = document.getElementById("LiveAcccount");
+    target.style.display = "block";
+  };
+
+  const Demo = () => {
+    let defaultAccount = document.getElementById("defaultAccount");
+    defaultAccount.style.display = "none";
+
+    let target = document.getElementById("LiveAcccount");
+    target.style.display = "none";
+
+    let Demo = document.getElementById("DemoAcccount");
+    Demo.style.display = "block";
+  };
+
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
+  
+
+
+  return (
+    <>
+      <div>
+        <div className="false-navbar">
+          <p style={{ marginRight: "3%" }}>
+            <Dropdown>
+              <Dropdown.Toggle
+                style={{
+                  background: "transparent",
+                  border: "none",
+                  color: "#0E155A",
+                  textAlign: "center",
+                }}
+                id="dropdown-basic"
+              >
+                Account
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => Live()}>Live</Dropdown.Item>
+                <Dropdown.Item onClick={() => Demo()}>Demo</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </p>
+
+          <p
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              width: "250px",
+              border: "2px solid #041668",
+              backgroundColor: "#041668",
+              padding: "5px",
+              borderRadius: "15px",
+              marginRight: "1rem",
+            }}
+          >
+            {" "}
+            <AiOutlineSearch style={{ width: "25px", height: "25px" }} />{" "}
+            <input type="search" placeholder="Search" className="Search" />
+            <IoMdArrowDropdown style={{ width: "25px", height: "25px" }} />
+          </p>
+          <p
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              width: "250px",
+              border: "2px solid #041668",
+              backgroundColor: "#041668",
+              padding: "5px",
+              borderRadius: "15px",
+              // marginRight: "5%",
+              marginRight: "1rem",
+            }}
+          >
+            {" "}
+            <AiOutlineSearch style={{ width: "25px", height: "25px" }} />
+            <input
+              type="search"
+              placeholder=" GraphSearch"
+              className="Search"
+            />
+            <IoMdArrowDropdown style={{ width: "25px", height: "25px" }} />
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ .history/src/Homepage/Sidebar/Sidebar_20230417144507.jsx
@@ -0,0 +1,588 @@
+/** @format */
+
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { VscAccount } from "react-icons/vsc";
+import Dropdown from "react-bootstrap/Dropdown";
+import { BsCashCoin } from "react-icons/bs";
+import { AiOutlineLogin } from "react-icons/ai";
+import { AiFillSetting } from "react-icons/ai";
+import { GrMoney } from "react-icons/gr";
+import { FaHandHoldingUsd } from "react-icons/fa";
+import { GiProfit } from "react-icons/gi";
+import { Button, Form, Modal } from "react-bootstrap";
+
+const Sidebar = ({ width }) => {
+  const show = () => {
+    let target = document.getElementById("HideText");
+    target.style.display = "block";
+  };
+
+  const show2 = () => {
+    let target = document.getElementById("NewText");
+    target.style.display = "block";
+  };
+
+  const firstTab = () => {
+    let target = document.getElementById("clearP");
+    target.style.display = "block";
+  };
+  const secondTab = () => {
+    let target = document.getElementById("clear2");
+    target.style.display = "block";
+  };
+  const thirdTab = () => {
+    let target = document.getElementById("clear3");
+    target.style.display = "block";
+  };
+  const fourthTab = () => {
+    let target = document.getElementById("clear4");
+    target.style.display = "block";
+  };
+
+
+  const [ modalShow , setModalShow ] = useState(false)
+
+  // Session Id
+  const SessionId = localStorage.getItem("sessionId");
+
+  function LogOut() {
+    localStorage.clear();
+    window.location.reload();
+  }
+
+
+  // Modal
+  
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form >
+            <Form.Group className="mb-3">
+              <Form.Label>User Id</Form.Label>
+              <Form.Control
+                type="number"
+                 />
+            </Form.Group>
+            <Button type="submit" variant="outline-success">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
+  return (
+    <>
+
+    <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false) } />
+       <div
+        className={
+          width ? "tem false-sidebar block" : "true-side false-sidebar "
+        }
+      >
+        <div className="false-logo">Logo fx</div>
+        <div style={{ textAlign: "center" }}>
+          <Dropdown>
+            <Dropdown.Toggle
+              style={{
+                backgroundColor: "#041668",
+                border: "2px solid #041668",
+                marginBottom: "15%",
+              }}
+              id="dropdown-basic"
+            >
+              Window tab Filter
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu
+              style={{ height: "200px", width: "190px", textAlign: "center" }}
+            >
+              <Dropdown.Item onClick={() => firstTab()}>
+                First Tab
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => secondTab()}>
+                Second Tab
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => thirdTab()}>
+                Third Tab
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => fourthTab()}>
+                {" "}
+                Fourth Tab
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+        {/* Default Account */}
+
+        <div
+          className="false-sidebar-menu"
+          style={{
+            border: "2px solid  #a4c0ed",
+            margin: "5%",
+            backgroundColor: "#a4c0ed",
+            padding: "15%",
+            minHeight: "500px",
+            borderRadius: "10px",
+          }}
+          id="defaultAccount"
+        >
+          <p
+            style={{
+              color: "#0E155A",
+              fontSize: "1.4rem",
+              textAlign: "center",
+            }}
+          >
+            Profile
+          </p>
+
+          <div>
+            {" "}
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+              onClick={() => show()}
+            >
+              {" "}
+              <VscAccount style={{ width: " 20px", height: "20px" }} /> Your
+              Account
+            </div>
+            <span
+              style={{
+                marginLeft: "60%",
+                fontSize: "13px",
+                fontWeight: "600",
+                display: "none",
+                cursor: "pointer",
+              }}
+              id="HideText"
+            >
+              {" "}
+              (Rs 800){" "}
+            </span>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "14px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+              onClick={() => show2()}
+            >
+              {" "}
+              <VscAccount style={{ width: " 20px", height: "20px" }} /> Demo
+              Account
+            </div>
+            <span
+              style={{
+                marginLeft: "60%",
+                fontSize: "13px",
+                fontWeight: "600",
+                display: "none",
+                cursor: "pointer",
+              }}
+              id="NewText"
+            >
+              {" "}
+              (Rs 20){" "}
+            </span>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                cursor: "pointer",
+                marginTop: "40px",
+              }}
+            >
+              <BsCashCoin style={{ width: " 20px", height: "20px" }} /> Cashback
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <AiFillSetting style={{ width: " 20px", height: "20px" }} />{" "}
+              Setting
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <BsCashCoin style={{ width: " 20px", height: "20px" }} />
+              BUY
+            </div>
+            {SessionId ? (
+              <div
+                style={{
+                  display: "flex",
+                  gap: "6px",
+                  fontSize: "16px",
+                  marginTop: "40px",
+                  cursor: "pointer",
+                }}
+                onClick={() => LogOut()}
+              >
+                <AiOutlineLogin style={{ width: " 20px", height: "20px" }} />{" "}
+                Logout
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+
+        {/* ----Default Account -------- */}
+
+        {/* Live Account */}
+
+        <div
+          className="false-sidebar-menu"
+          style={{
+            border: "2px solid  #a4c0ed",
+            margin: "5%",
+            backgroundColor: "#a4c0ed",
+            padding: "15%",
+            minHeight: "500px",
+            borderRadius: "10px",
+            display: "none",
+          }}
+          id="LiveAcccount"
+        >
+          <p
+            style={{
+              color: "#0E155A",
+              fontSize: "1.4rem",
+              textAlign: "center",
+            }}
+          >
+            Profile
+          </p>
+          <p style={{ textAlign: "center", color: "black" }}>Live Account</p>
+          <div>
+            {" "}
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <GrMoney style={{ width: " 20px", height: "20px" }} /> Balance
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "14px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <BsCashCoin style={{ width: " 20px", height: "20px" }} /> CashBack
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                cursor: "pointer",
+                marginTop: "40px",
+              }}
+            >
+              <FaHandHoldingUsd style={{ width: " 20px", height: "20px" }} />{" "}
+              Holdings
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <GiProfit style={{ width: " 20px", height: "20px" }} /> Portfolio
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              {" "}
+              <AiFillSetting style={{ width: " 20px", height: "20px" }} />{" "}
+              Settings
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <AiOutlineLogin style={{ width: " 20px", height: "20px" }} />{" "}
+              LogOut
+            </div>
+          </div>
+        </div>
+        {/* ------- Live Account ---------- */}
+
+        {/* Demo Account */}
+        <div
+          className="false-sidebar-menu"
+          style={{
+            border: "2px solid  #a4c0ed",
+            margin: "5%",
+            backgroundColor: "#a4c0ed",
+            padding: "15%",
+            minHeight: "500px",
+            borderRadius: "10px",
+            display: "none",
+          }}
+          id="DemoAcccount"
+        >
+          <p
+            style={{
+              color: "#0E155A",
+              fontSize: "1.4rem",
+              textAlign: "center",
+            }}
+          >
+            Profile
+          </p>
+
+          <p style={{ textAlign: "center", color: "black" }}>Demo Account</p>
+
+          <div>
+            {" "}
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <GrMoney style={{ width: " 20px", height: "20px" }} /> Balance
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "14px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <BsCashCoin style={{ width: " 20px", height: "20px" }} /> Balance
+              refill
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                cursor: "pointer",
+                marginTop: "40px",
+              }}
+            >
+              <FaHandHoldingUsd style={{ width: " 20px", height: "20px" }} />{" "}
+              Holdings
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <GiProfit style={{ width: " 20px", height: "20px" }} /> Portfolio
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              {" "}
+              <AiFillSetting style={{ width: " 20px", height: "20px" }} />{" "}
+              Settings
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              <AiOutlineLogin style={{ width: " 20px", height: "20px" }} />{" "}
+              LogOut
+            </div>
+          </div>
+        </div>
+        {/* ---------------Demo Account ----------- */}
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
+
+{
+  /* <div
+          className="false-sidebar-menu"
+          style={{
+            border: "2px solid  #a4c0ed",
+            margin: "5%",
+            backgroundColor: "#a4c0ed",
+            padding: "15%",
+            minHeight: "500px",
+            borderRadius: "10px",
+          }}
+        >
+          <p
+            style={{
+              color: "#0E155A",
+              fontSize: "1.4rem",
+              textAlign: "center",
+            }}
+          >
+            Profile
+          </p>
+
+          <div>
+            {" "}
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+              onClick={() => show()}
+            >
+              {" "}
+              {menuItem[0].img} {menuItem[0].title}
+            </div>
+            <span
+              style={{
+                marginLeft: "60%",
+                fontSize: "13px",
+                fontWeight: "600",
+                display: "none",
+                cursor: "pointer",
+              }}
+              id="HideText"
+            >
+              {" "}
+              (Rs 800){" "}
+            </span>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "14px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+              onClick={() => show2()}
+            >
+              {" "}
+              {menuItem[1].img} {menuItem[1].title}
+            </div>
+            <span
+              style={{
+                marginLeft: "60%",
+                fontSize: "13px",
+                fontWeight: "600",
+                display: "none",
+                cursor: "pointer",
+              }}
+              id="NewText"
+            >
+              {" "}
+              (Rs 20){" "}
+            </span>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                cursor: "pointer",
+                marginTop: "40px",
+              }}
+            >
+              {menuItem[2].img} {menuItem[2].title}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              {menuItem[3].img} {menuItem[3].title}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "6px",
+                fontSize: "16px",
+                marginTop: "40px",
+                cursor: "pointer",
+              }}
+            >
+              {" "}
+              {menuItem[4].img} {menuItem[4].title}
+            </div>
+          </div>
+
+        </div> */
+}
